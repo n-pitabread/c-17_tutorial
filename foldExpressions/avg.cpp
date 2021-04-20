e3a90e9b7d3151bcf4d204d86c19322b3e308500
Puriwat Khantiviriya <p.khantiviriya@gmail.com>
2021-04-20 02:28:09 +0200

    Add comment to file

--- foldExpressions/avg.cpp
+++ foldExpressions/avg.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 
+// fold expression
 template<typename ... T>
 auto avg(T ... t) {
+    // sum all arguments and divided by the number of all arguments
     return (t + ...) / sizeof...(t);
 }
 
